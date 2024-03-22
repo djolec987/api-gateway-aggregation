f1825e1eab47e899492cb7423d388709b0f91d68
Djordje Cvetkovic <djolec987@gmail.com>
2024-03-22 23:47:09 +0100

    Implemented Kafka Integration

--- inventory-service/src/main/java/rs/codecraft/apigatewayaggregation/inventoryservice/adapter/messaging/kafka/KafkaMessagingService.java
+++ inventory-service/src/main/java/rs/codecraft/apigatewayaggregation/inventoryservice/adapter/messaging/kafka/KafkaMessagingService.java
@@ -1,7 +1,10 @@
 package rs.codecraft.apigatewayaggregation.inventoryservice.adapter.messaging.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import rs.codecraft.apigatewayaggregation.inventoryservice.core.entity.Product;
 import rs.codecraft.apigatewayaggregation.inventoryservice.core.service.MessagingService;
 
@@ -9,8 +12,9 @@ import rs.codecraft.apigatewayaggregation.inventoryservice.core.service.Messagin
 public class KafkaMessagingService implements MessagingService {
 
     @Override
-    @KafkaListener(id = "inventory_service_consumer", topics = "products")
+    @KafkaListener(id = "inventory_service_consumer", topics = "products", properties = {"spring.json.value.default.type=rs.codecraft.apigatewayaggregation.inventoryservice.core.entity.Product"})
     public void processProductAdded(Product product) {
         System.out.println(product);
     }
+
 }
--- product-service/src/main/java/rs/codecraft/apigatewayaggregation/productservice/adapter/messaging/kafka/KafkaMessagingService.java
+++ product-service/src/main/java/rs/codecraft/apigatewayaggregation/productservice/adapter/messaging/kafka/KafkaMessagingService.java
@@ -1,5 +1,7 @@
 package rs.codecraft.apigatewayaggregation.productservice.adapter.messaging.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Builder;
 import org.springframework.kafka.core.KafkaTemplate;
 import rs.codecraft.apigatewayaggregation.productservice.core.entity.Product;
@@ -12,6 +14,14 @@ public class KafkaMessagingService implements MessagingService {
 
     @Override
     public void sendProductAddedMessage(Product product) {
+//        ObjectMapper om = new ObjectMapper();
+//        String productAsString = null;
+//        try {
+//            productAsString = om.writeValueAsString(product);
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException("Error converting product " + product + " to JSON string.", e);
+//        }
+//        kafkaTemplate.send("products", "products_service", productAsString);
         kafkaTemplate.send("products", "products_service", product);
     }
 }
